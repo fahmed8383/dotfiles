cb9b328592604176337e98c806c31299f5052926
fahmed8383 <fahmed4030@gmail.com>
2022-04-27 23:51:20 -0400

    conky start up script updated

--- conky/conky.sh
+++ conky/conky.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+# Terminate already running conky instances
+killall -q conky
+
+# Wait until the processes have been shut down
+while pgrep -u $UID -x conky >/dev/null; do sleep 1; done
+
+# Launch conky
 conky -c $(dirname $0)/clock.conf &
 conky -c $(dirname $0)/cpu_ram.conf &
 conky -c $(dirname $0)/stocks.conf &
